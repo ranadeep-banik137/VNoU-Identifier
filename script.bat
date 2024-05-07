a67794ba370accf8beb6f082c4b6f03829892499
ranadeep-banik137 <42244847+ranadeep-banik137@users.noreply.github.com>
2024-05-07 13:09:55 +0530

    bat file arranged to start from windows system

--- script.bat
+++ script.bat
@@ -1,2 +1,28 @@
 @echo off
-echo Starting the application
+echo Starting the application....
+
+setx LOG_LEVEL="INFO"
+setx CAMERA_INDEX=0
+
+echo Setting envionment variables...
+
+pip install --upgrade setuptools
+py -m ensurepip --upgrade
+py -m pip install --upgrade pip
+
+echo Installing basic pip libraries....
+
+start cmd /k "python main.py"
+echo Starting Face Identifier....
+
+start cmd /k "python file-transfer.py"
+echo Starting Database Transfer....
+
+start cmd /k "nodemon gui/server.js"
+echo Starting the server....
+
+echo Opening Google Chrome...
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:9000
+echo Link VnoU-Identifier opened in Google Chrome
+
+pause
