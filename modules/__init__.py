f939f55a0c0eb98f6a22a2e2ce9f76ca0c00954c
ranadeep-banik137 <42244847+ranadeep-banik137@users.noreply.github.com>
2024-05-13 13:35:06 +0530

    added module init file

---
+++ modules/__init__.py
@@ -0,0 +1,16 @@
+# modules/__init__.py
+
+# You can import modules from the package
+from .speech import *
+from .data_cache import *
+from .data_reader import *
+from .data_transfer import *
+from .database import *
+from .date_time_converter import *
+from .event_data_handler import *
+from .exportdb import *
+from .face_identifier import *
+
+# You can define any initialization code here if needed
+# For example:
+print("Initializing the 'modules' package...")
